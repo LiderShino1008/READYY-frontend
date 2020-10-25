179eb0f47f9263c7f8a006f2e54100e4f55fc541
LiderShino1008 <walterghost@live.com>
2020-10-24 23:52:18 -0600

    [ADD] signup.html, landing images [EDIT] validate login fields

--- assets/js/controlador-login.js
+++ assets/js/controlador-login.js
@@ -1,3 +1,11 @@
 ﻿$(document).ready(function(){
-    console.log("El DOM 'login.html' ha sido cargado ♥");
+    console.log('El DOM login.html ha sido cargado ♥');
 });
+
+function iniciarSesion() {
+    let valEmail = validarRegex('login-txt-email','email');
+    let valPassword = validarRegex('login-txt-password','password');
+    if (valEmail && valPassword) {
+        alert('Inicio de sesión exitoso');
+    }
+}
