a5042d7fc7ea7a22d887cf56877aebb43dd0d1d3
LiderShino1008 <walterghost@live.com>
2020-11-14 09:12:08 -0600

    [ADD] bootstrap modal windows [EDIT] signup: toggle user type fields

--- assets/js/controlador-signup.js
+++ assets/js/controlador-signup.js
@@ -1,47 +1,25 @@
-$(document).ready(function(){
+﻿$(document).ready(function(){
     console.log('El DOM signup.html ha sido cargado ♥');
 });
 
 /**
  * Mostrar en pantalla los campos necesarios para registrarse de acuerdo
  * al tipo de usuario seleccionado.
- * @param {String} tipo Tipo de usuario
  */
-function mostrarUT(tipo) {
-    switch (tipo) {
-        case 'cliente':
-            // Ocultar campos de administrador de negocios
-            document.getElementById('reg-div-adm').classList.remove('d-flex');
-            document.getElementById('reg-div-adm').classList.add('d-none');
-            // Mostrar campos de cliente
-            if (document.getElementById('reg-div-cliente').classList.contains('d-none')) {
-                document.getElementById('reg-div-cliente').classList.remove('d-none');
-                document.getElementById('reg-div-cliente').classList.add('d-flex');
-            }
-            // Si no esta visible mostrar boton para registrarse
-            if (document.getElementById('reg-div-btn').classList.contains('d-none')) {
-                document.getElementById('reg-div-btn').classList.remove('d-none');
-                document.getElementById('reg-div-btn').classList.add('d-flex');
-            }
-            break;
-
-        case 'adm':
-            // Mostrar campos de cliente
-            if (document.getElementById('reg-div-cliente').classList.contains('d-none')) {
-                document.getElementById('reg-div-cliente').classList.remove('d-none');
-                document.getElementById('reg-div-cliente').classList.add('d-flex');
-            }
-            // Mostrar campos de administrador de negocios
-            document.getElementById('reg-div-adm').classList.remove('d-none');
-            document.getElementById('reg-div-adm').classList.add('d-flex');
-            // Mostrar boton para registrarse
-            if (document.getElementById('reg-div-btn').classList.contains('d-none')) {
-                document.getElementById('reg-div-btn').classList.remove('d-none');
-                document.getElementById('reg-div-btn').classList.add('d-flex');
-            }
-            break;
+$('input[name="registro-userType"]').click(function() {
+  if($(this).attr('value') == 'Cliente') {
+        $('#reg-div-adm').hide();
+        $('#reg-div-cliente').show();
     }
-}
+
+    else {
+
+        $('#reg-div-adm').show();
+        $('#reg-div-cliente').show();
+   
+ }
+
+});
 
 /**
  * Obtener el valor de un grupo de radio button por nombre.
